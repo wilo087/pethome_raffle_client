3b33971df61e4ddde8832ad6c55bfe32db640b03
Wilowayne De La Cruz G <wilo0087@gmail.com>
2020-05-13 22:20:37 -0400

    Setting up Eslint rules for React - TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'dot-notation': 0,
+    'react/display-name': 0,
+    "no-console": ["error"],
+    'no-underscore-dangle': 0,
+    'class-methods-use-this': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'lines-between-class-members': 0,
+    '@typescript-eslint/camelcase': 0,
+    'max-len': ["error", { "code": 120 }],
+    'react/static-property-placement': 0,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/explicit-function-return-type": ["error"],
+    "import/order": ["error", {
+      "newlines-between": "always",
+      "groups": [
+        ["builtin", "external", "internal"],
+        ["parent", "sibling"],
+        "index",
+      ],
+    }],
+  },
+};
