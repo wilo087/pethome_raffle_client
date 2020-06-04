fd49d038ff341dbd36f48d775eac908af0f6dc31
Adrian Mendez <mendezadrian149@gmail.com>
2020-06-04 19:01:13 -0400

    moving JSX to isolate Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import './assets/styles/App.scss';
-import carImage from './assets/images/car@2x.png';
-import logoPetHome from './assets/images/pethomeRD@2x.png';
-import logoNavi from './assets/images/navi@2x.png';
-import Dog from './components/Dog/Dog';
+import Home from './components/Home/Home'
 
-const App: React.FC = () => {
+
+const App: React.FC = (): JSX.Element => (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [winner, setWinner] = React.useState(0);
+  // const [winner, setWinner] = React.useState(0);
+  <Router>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>
 
-  return (
-    <>
-      <div className="w7e-left">
-        <h1>
-          Danos
-          <br />
-          Una pata
-        </h1>
-        <h2>
-          Para construir un albergue
-          <br />
-          de mascotas que sea seguro,
-          <br />
-          amplio y esté lleno de amor.
-        </h2>
-
-        <div className="w7e-logos">
-          <div className="w7e-pethome">
-            <img src={logoPetHome} alt="@petHomeRD" />
-          </div>
-          <div className="w7e-navi">
-            <img src={logoNavi} alt="@navi69" />
-          </div>
-        </div>
-        {/* <button type="button" className="w7e" onClick={(): void => setWinner(winner + 1)}>
-          Buscar ganador
-        </button> */}
-
-      </div>
-      <div className="w7e-right">
-        {/* <div className="w7e-winner"> En minutos, esteHonda Fit 2015 puede ser tuyo </div> */}
-        <div className="w7e-winner w7e-looking">
-          <div className="w7e-left">
-            <Dog />
-          </div>
-
-          <div className="w7e-right">
-            Buscando Ganador
-          </div>
-        </div>
-        <div className="w7e-car">
-          <img src={carImage} width="845" height="413" alt="Honda Fix 2015" />
-        </div>
-      </div>
-      <footer />
-    </>
-  );
-};
+)
 
 export default App;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+// import carImage from '../assets/images/car@2x.png';
+import carImage from '../../assets/images/car@2x.png';
+import logoPetHome from '../../assets/images/pethomeRD@2x.png';
+import logoNavi from '../../assets/images/navi@2x.png';
+import Dog from '../Dog/Dog';
+
+const Home: React.FC = (): JSX.Element => (
+  <>
+    <div className="w7e-left">
+      <h1>
+        Danos
+        <br />
+        Una pata
+      </h1>
+      <h2>
+        Para construir un albergue
+        <br />
+        de mascotas que sea seguro,
+        <br />
+        amplio y esté lleno de amor.
+      </h2>
+
+      <div className="w7e-logos">
+        <div className="w7e-pethome">
+          <img src={logoPetHome} alt="@petHomeRD" />
+        </div>
+        <div className="w7e-navi">
+          <img src={logoNavi} alt="@navi69" />
+        </div>
+      </div>
+      {/* <button type="button" className="w7e" onClick={(): void => setWinner(winner + 1)}>
+        Buscar ganador
+      </button> */}
+
+    </div>
+    <div className="w7e-right">
+      {/* <div className="w7e-winner"> En minutos, esteHonda Fit 2015 puede ser tuyo </div> */}
+      <div className="w7e-winner w7e-looking">
+        <div className="w7e-left">
+          <Dog />
+        </div>
+
+        <div className="w7e-right">
+          Buscando Ganador
+        </div>
+      </div>
+      <div className="w7e-car">
+        <img src={carImage} width="845" height="413" alt="Honda Fix 2015" />
+      </div>
+    </div>
+    <footer />
+  </>
+)
+
+export default Home;
