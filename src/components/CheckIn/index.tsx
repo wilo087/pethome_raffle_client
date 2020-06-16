99f965855e4a49f7a94c0dc015478af968e2d3ab
Adrian Mendez <mendezadrian149@gmail.com>
2020-06-16 19:25:18 -0400

    working on form registration

--- src/components/CheckIn/index.tsx
+++ src/components/CheckIn/index.tsx
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, } from 'react';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+import { Error } from '../../interfaces'
+import { Alert } from '@material-ui/lab';
+
+
 import './checkin.scss';
+import Loading from '../Loading';
+
+
+const SET_CHECKIN = gql`
+  mutation createUser($data: createUserInput!){
+    createUser(data: $data){
+      name,
+      identificationCard,
+      code,
+  }
+}`;
 
 const CheckIn: React.FC = (): JSX.Element => {
+  const [setCheckIn, { loading, data: mutationData }] = useMutation(SET_CHECKIN, {
+    ignoreResults: false,
+  });
+  const [error, setError] = useState('');
+
   const [data, setData] = useState({
     name: '',
     code: '',
@@ -17,70 +39,113 @@ const CheckIn: React.FC = (): JSX.Element => {
     }));
   };
 
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+
+    try {
+      await setCheckIn({
+        variables: {
+          data: {
+            name: data.name,
+            code: Number(data.code),
+            identificationCard: data.identificationCard,
+          },
+        },
+      });
+
+      setError('');
+
+    } catch (error) {
+      error.graphQLErrors.forEach((err: Error) => {
+        if (err.extensions.code !== 'INTERNAL_SERVER_ERROR') {
+          return setError(err.message)
+        } else {
+          setError('an error occurred please try again later')
+        }
+      })
+    }
+  };
+
   return (
-    <div className="form">
-      <div id="signup">
-        <h1>Register Participant</h1>
-        <form action="/" method="post">
-          <div className="top-row">
+    <div>
+      {mutationData && (
+        <Alert
+          severity="success">
+          the user {mutationData.createUser.name} is created
+        </Alert>
+      )}
 
-            <div className="field-wrap">
-              <label htmlFor="name">
-                Name
+      {error && (<Alert
+        severity="error">
+        {error}
+      </Alert>)}
+
+      <div className="form">
+        <div id="signup">
+          <h1>Register Participant</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="top-row">
+
+              <div className="field-wrap">
+                <label htmlFor="name">
+                  Name
                 {' '}
-                <span className="req">*</span>
-              </label>
+                  <span className="req">*</span>
+                </label>
 
-              <input
-                id="name"
-                type="text"
-                required
-                autoComplete="off"
-                onChange={handleChange}
-                value={data.name}
-              />
-            </div>
+                <input
+                  name="name"
+                  type="text"
+                  required
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={data.name}
+                />
+              </div>
 
-            <div className="field-wrap">
-              <label htmlFor="code">
-                Code
+              <div className="field-wrap">
+                <label htmlFor="code">
+                  Code
                 {' '}
-                <span className="req">*</span>
-              </label>
+                  <span className="req">*</span>
+                </label>
 
-              <input
-                id="code"
-                type="text"
-                required
-                autoComplete="off"
-                onChange={handleChange}
-                value={data.code}
-              />
+                <input
+                  name="code"
+                  type="number"
+                  required
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={data.code}
+                />
 
+              </div>
             </div>
-          </div>
-          <div className="field-wrap">
-            <label htmlFor="identificationCard">
-              Identification Card
+            <div className="field-wrap">
+              <label htmlFor="identificationCard">
+                Identification Card
               {' '}
-              <span className="req">*</span>
-            </label>
+                <span className="req">*</span>
+              </label>
 
-            <input
-              id="identificationCard"
-              type="text"
-              required
-              autoComplete="off"
-              onChange={handleChange}
-              value={data.identificationCard}
-            />
+              <input
+                name="identificationCard"
+                type="text"
+                required
+                autoComplete="off"
+                onChange={handleChange}
+                value={data.identificationCard}
+              />
 
-          </div>
-          <button type="submit" className="button button-block">
-            Save
+            </div>
+            <button type="submit" className="button button-block">
+              Save
           </button>
-        </form>
-      </div>
+          </form>
+        </div>
+        {loading && <Loading />}
+      </div >
     </div>
   );
 };
